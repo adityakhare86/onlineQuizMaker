88021248baddf4156e7b0c4b08aa309b6666d854
adityakhare86 <adityakhare863@gmail.com>
2024-07-06 02:37:49 +0530

    updated ui of registration page

--- client/src/components/Authentication/Registration.jsx
+++ client/src/components/Authentication/Registration.jsx
@@ -50,97 +50,60 @@ class Registration extends Component {
           checkLogin={this.props.checkLogin}
           onLogout={this.props.onLogout}
         />
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-8 offset-sm-4 mt-5">
-              <Link to="/">
-                <span className="back-to-home ">
-                  Back to Home
-                </span>
-              </Link>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-8 offset-sm-4">
-              <div className="auth-title mt-1">Register for a New Account</div>
-            </div>
-          </div>
+        <div className="container-fluid container-fluid-registration">
+          <div className="auth-title mt-1">Register for a New Account</div>
           <form onSubmit={this.handleRegistrationSubmit}>
-            <div className="form-group">
+            <div className="form-group registration-form">
               {/* name */}
-              <div className="row mt-4">
-                <div className="col-sm-4 offset-sm-4">
-                  <label className="input-label" htmlFor="inputName">
-                    Name
-                  </label>
-                  <input
-                    required="required"
-                    type="text"
-                    className="form-control input-field"
-                    aria-describedby="emailInput"
-                    placeholder="John Doe"
-                    value={this.state.name}
-                    onChange={this.handleNameChange}
-                  />
-                </div>
-              </div>
-
-              <div className="row mt-4">
-                <div className="col-sm-4 offset-sm-4">
-                  <label className="input-label" htmlFor="inputEmail">
-                    Email address
-                  </label>
-                  <input
-                    required="required"
-                    type="email"
-                    className="form-control input-field"
-                    aria-describedby="emailInput"
-                    placeholder="johndoe@email.com"
-                    value={this.state.email}
-                    onChange={this.handleEmailChange}
-                  />
-                </div>
-              </div>
-
-              <div className="row mt-4">
-                <div
-                  className="col-sm-4 offset-sm-4"
+              <label className="input-label" htmlFor="inputName">
+                Name
+              </label>
+              <input
+                required="required"
+                type="text"
+                className="form-control input-field registration-form-placeholders"
+                aria-describedby="emailInput"
+                placeholder="John Doe"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+              />
+              <label className="input-label" htmlFor="inputEmail">
+                Email address
+              </label>
+              <input
+                required="required"
+                type="email"
+                className="form-control input-field registration-form-placeholders"
+                aria-describedby="emailInput"
+                placeholder="johndoe@email.com"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+              />
+              <label className="input-label" htmlFor="inputPassword">
+                Password
+              </label>
+              <input
+                required="required"
+                type="password"
+                className="form-control input-field registration-form-placeholders"
+                aria-describedby="passwordInput"
+                placeholder="strong password"
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+              />
+              <div className="registration-form-button-cont">
+                <button
+                  type="submit"
+                  className="button registration-form-register-button"
+                  onClick={this.handleRegistration}
                 >
-                  <label className="input-label" htmlFor="inputPassword">
-                    Password
-                  </label>
-                  <input
-                    required="required"
-                    type="password"
-                    className="form-control input-field"
-                    aria-describedby="passwordInput"
-                    placeholder="strong password"
-                    value={this.state.password}
-                    onChange={this.handlePasswordChange}
-                  />
-                </div>
-              </div>
-              <div className="row mt-5">
-                <div className="col-sm-2 offset-sm-4">
-                  <button
-                    type="submit"
-                    className="button register-button"
-                    style={{
-                      width: "100%",
-                      height: "42px",
-                    }}
-                    onClick={this.handleRegistration}
-                  >
-                    Register
-                  </button>
-                </div>
-                <div className="col-sm-2">
-                  <Link to="/login">
-                    <div className="back-to-home mt-2">
-                      Already Registered? Login
-                    </div>
-                  </Link>
-                </div>
+                  Register
+                </button>
+                <Link to="/login">
+                  <div className="already-registered button">
+                    Already Registered? Login
+                  </div>
+                </Link>
               </div>
               {this.state.error && (
                 <AuthError text="Looks like you made a mistake. The email is probably taken." />
