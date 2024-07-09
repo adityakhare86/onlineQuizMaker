2352604062f6f6799bb150d8fe1f1069f6cdd613
adityakhare86 <adityakhare863@gmail.com>
2024-07-09 13:40:09 +0530

    user verification

--- app/controller/AuthController.js
+++ app/controller/AuthController.js
@@ -82,6 +82,7 @@ const AuthController = {
     const token = req.header("auth-token");
     if (!token) return res.status(401).send("Access Denied");
     try {
+      console.log("Verifying user");
       const verified = jwt.verify(token, process.env.JWT_SECRET);
       req.user = verified;
       console.log("user verified");
